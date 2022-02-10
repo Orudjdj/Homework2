ed55dd443b1150b23ef2d5ae5ecec02edb9063d4
Asus <Asus@DESKTOP-ETU053B>
2022-02-10 10:07:30 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 214;
+                int remain =0;
+            while (number != 0)
+            {
+                remain += number % 10;
+                number = number / 10;
+            }
+            Console.WriteLine(remain);  
+        }
+
+        
+       
+    }
+}
